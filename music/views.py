fa16a21755575bd813d304bfa6df424336e1487a
adriano-ferrari <adriano-ferrari@outlook.com>
2019-06-07 15:27:09 -0300

    Incluídos os arquivos: music/static/music/images/background.jpg

--- music/views.py
+++ music/views.py
@@ -1,6 +1,5 @@
-from django.http import Http404
-from django.shortcuts import render
-from .models import Album
+from django.shortcuts import render, get_object_or_404
+from .models import Album, Song
 
 
 def index(request):
@@ -9,8 +8,10 @@ def index(request):
 
 
 def detail(request, album_id):
-    try:
-        album = Album.objects.get(pk=album_id)
-    except Album.DoesNotExist:
-        raise Http404("Album does not exist")
+    album = get_object_or_404(Album, pk=album_id)
+    return render(request, 'music/detail.html', {'album': album})
+
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/detail.html', {'album': album})
